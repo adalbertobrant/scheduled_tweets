e4776fb22b36846653ebc4cabc242ba031545e15
adalbertobrant <adalbertobrant@gmail.com>
2021-02-07 17:51:47 -0300

    WIP forms, model db, registrations, route

---
+++ app/models/user.rb
@@ -0,0 +1,12 @@
+# email:string
+# password_digest:string
+#
+# password:string virtual
+# password_confirmation:string virtual
+
+class User < ApplicationRecord
+    has_secure_password
+
+    validates :email, presence: true, format: { with: /\A[^@\s]+@[^@\s]+\z/, message: "must be a valid e-mail address"}
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,13 @@ Rails.application.routes.draw do
   # GET /about
   get "about", to: "about#index"
 
+  # GET /sign-up
+  get "sign_up", to: "registrations#new"
+  post "sign_up", to: "registrations#create"
+
   # GET root
  # get "/", to: 'main#index'
   root to:"main#index"
+
+
 end
