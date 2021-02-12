343b0791b1ffc2292dd51791bb7c1e39cca49e85
adalbertobrant <adalbertobrant@gmail.com>
2021-02-12 06:48:43 -0300

    WIP routes for password reset and mailer configuration

--- app/controllers/password_resets_controller.rb
+++ app/controllers/password_resets_controller.rb
@@ -3,5 +3,16 @@ class PasswordResetsController < ApplicationController
     end
 
     def create
+      @user = User.find_by(email: params[:email])
+      if @user.present?
+        #send email
+        #send email later
+        #PasswordMailer.with(user: @user).reset.deliver_later   
+        #sendo email now -this one is not very good for speed, takes too long     
+        PasswordMailer.with(user: @user).reset.deliver_now
+
+          
+      end
+      redirect_to root_path, notice: "If an account with that email was found, link was sent to reset your password!"
     end
 end
--- config/environments/development.rb
+++ config/environments/development.rb
@@ -73,4 +73,10 @@ Rails.application.configure do
 
   # Uncomment if you wish to allow Action Cable access from any origin.
   # config.action_cable.disable_request_forgery_protection = true
+
+  #send mail
+  #config.action_mailer.default_url_options = { :host => "example.com" }
+  #config.action_mailer.default_url_options = {:host => "localhost:3000"} #stackoverflow
+  config.action_mailer.default_url_options = { host: "localhost:3000"}
+
 end
